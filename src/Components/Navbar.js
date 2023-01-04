172d21c055257f39d44c6e86314c8f7de3f7cd48
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-01-04 22:49:04 +0800

    add <Link> to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HiOutlineShoppingBag } from 'react-icons/hi';
+import { HiOutlineShoppingBag, HiOutlineShoppingCart } from 'react-icons/hi';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -10,12 +11,17 @@ const Navbar = () => {
             </div>
             <div className='nav-link'>
                 <ul>
-                    <li>home</li>
-                    <li>about</li>
-                    <li>shop</li>
+                    <li>
+                        <Link to="/index" target="_blank">home</Link>
+                    </li>
+                    <li>
+                        <Link to="/index" target="_blank">about</Link>
+                    </li>
+                    <li>
+                        <Link to="/index" target="_blank"><HiOutlineShoppingCart size={30} /></Link>
+                    </li>
                 </ul>
             </div>
-
         </div>
 
     )
