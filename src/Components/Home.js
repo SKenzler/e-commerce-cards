c4cd3529aaf9f483a074e3ab31199ad85e49514b
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-01-06 10:33:40 +0800

    add a home page to the e-coomerce app

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import heroImage from '../images/hero-shoe-image.jpg'
 
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className='hero-section'>
+            <div className='hero-content'>
+                <h3>Get your running shoes on</h3>
+                <h1>It's time to shop</h1>
+                <p>Laborum eiusmod do nisi ea occaecat labore
+                    cupidatat ipsum laborum est reprehenderit mollit aute eiusmod. Ad laborum
+                    exercitation voluptate est adipisicing duis duis minim commodo cillum in.
+                    Aliquip dolor pariatur sit quis laborum. Excepteur deserunt mollit est reprehenderit
+                    cupidatat ea deserunt. Proident aliquip adipisicing exercitation enim magna cupida.</p>
+            </div>
+            <div className='img-box'>
+                <img src= {heroImage} className='hero-img' alt="A runner putting on shoes" />
+            </div>
+            
+        </section>
      );
 }
 
