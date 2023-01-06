fb08d1536058e7dc499be6999337c48417b5ae74
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-01-06 16:31:22 +0800

    make home page more responsive

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import heroImage from '../images/hero-shoe-image.jpg'
+import { HiOutlineShoppingCart } from 'react-icons/hi';
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return ( 
@@ -12,6 +14,9 @@ const Home = () => {
                     exercitation voluptate est adipisicing duis duis minim commodo cillum in.
                     Aliquip dolor pariatur sit quis laborum. Excepteur deserunt mollit est reprehenderit
                     cupidatat ea deserunt. Proident aliquip adipisicing exercitation enim magna cupida.</p>
+                    <Link to='/Card'>
+                        <button type='button' className='btn-cart'><HiOutlineShoppingCart size={25} />&nbsp; Start Shopping</button>
+                    </Link>
             </div>
             <div className='img-box'>
                 <img src= {heroImage} className='hero-img' alt="A runner putting on shoes" />
