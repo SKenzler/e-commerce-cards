6b1b0ae6d82ed16c5006a8e22dba25698ed7e71a
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-01-09 22:02:18 +0800

    Debug error in data.js

--- src/data.js
+++ src/data.js
@@ -4,6 +4,8 @@ const data = [
         stockStatus: 0,
         image: "nike-blazer.png",
         description: "Esse ad excepteur quis exercitation enim aliquip veniam est tempor fugiat dolore voluptate.",
-        price: 240,
+        price: 240
     }
 ];
+
+export default data;
